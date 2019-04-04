9b9ecb640ea64c165edbccc9a6d2b544abffb845
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-04-04 22:31:49 +0100

    isogram: remove list comp, it's a bit redundant, maybe slightly faster now

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -3,14 +3,17 @@ def is_isogram(string):
     if not string:
         return True
 
-    # Extract non-space and non-hypen chars as lower case
-    chars = [x.lower() for x in string if x != ' ' and x != '-']
-
     # Use a set for fast lookup
     unique_chars = set()
 
     # Look for duplicates so far
-    for c in chars:
+    for c in string:
+        # Skip non-space and non-hypen chars
+        if c == ' ' or c == '-':
+            continue
+
+        c = c.lower()
+
         if c in unique_chars:
             return False
 
