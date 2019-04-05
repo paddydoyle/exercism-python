4d2878e34e55e18f17367cec21f3c22e591112b1
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-04-05 22:05:08 +0100

    bank_account: complete solution; looks ok

--- bank-account/bank_account.py
+++ bank-account/bank_account.py
@@ -1,18 +1,69 @@
+from enum import Enum, auto
+from threading import Lock
+
+
 class BankAccount(object):
+
+    # Simple state machine
+    class State(Enum):
+        INIT = auto()
+        OPEN = auto()
+        CLOSED = auto()
+
     def __init__(self):
-        pass
+        # Must make state changes thread-safe via a mutex
+        self.state = self.State.INIT
+
+        # Must make balance changes thread-safe via a mutex
+        self.balance = 0
+
+        # Mutex. Think we can get away with a single lock, as deposit and
+        # withdrawal depend on the state, as well as the balance. Otherwise
+        # consider separate locks for state and balance.
+        self.lock = Lock()
 
     def get_balance(self):
-        pass
+        with self.lock:
+            if self.state != self.State.OPEN:
+                raise ValueError("Can only check balance of open accounts")
+
+            return self.balance
 
     def open(self):
-        pass
+        with self.lock:
+            if self.state == self.State.OPEN:
+                raise ValueError("Cannot re-open an open account")
+
+            self.state = self.State.OPEN
 
     def deposit(self, amount):
-        pass
+        with self.lock:
+            if self.state != self.State.OPEN:
+                raise ValueError("Can only check balance of open accounts")
+
+            if amount < 0:
+                raise ValueError("Can only deposit positive amounts")
+
+            self.balance += amount
 
     def withdraw(self, amount):
-        pass
+        with self.lock:
+            if self.state != self.State.OPEN:
+                raise ValueError("Can only withdraw from open accounts")
+
+            if amount < 0:
+                raise ValueError("Can only withdraw positive amounts")
+
+            if self.balance < amount:
+                raise ValueError("Unable to withdraw more than balance")
+
+            self.balance -= amount
 
     def close(self):
-        pass
+        with self.lock:
+            if self.state != self.State.OPEN:
+                raise ValueError("Can only close an open account")
+
+            self.state = self.State.CLOSED
+            # Closed accounts do not retain a balance.
+            self.balance = 0
