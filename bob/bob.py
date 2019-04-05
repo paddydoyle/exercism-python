d3d08eaaedaf8999f63dc5a7b249cab8c5d47a8e
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-04-05 23:07:30 +0100

    bob: ugh, forgot about simpler string methods

--- bob/bob.py
+++ bob/bob.py
@@ -1,27 +1,18 @@
-import re
-from string import ascii_uppercase, ascii_letters
-
-
 def hey(phrase):
-    if re.search(r'^\s*$', phrase):
+    phrase = phrase.strip()
+
+    if not phrase:
         # Empty string, or only whitespace
         return "Fine. Be that way!"
 
-    # Use a set intersection to find all of the chars in the string, both
-    # uppercase and lowercase.
-    chars = set(ascii_letters).intersection(phrase)
-
-    # Non-empty list of chars, and the uppercase set is a superset of them
-    all_uppercase = chars and set(ascii_uppercase).issuperset(chars)
-
-    if all_uppercase and re.search(r'\?\s*$', phrase):
+    if phrase.isupper() and phrase.endswith('?'):
         # All uppercase, followed by '?'
         return "Calm down, I know what I'm doing!"
-    elif re.search(r'\?\s*$', phrase):
-        # '?' at end
-        return "Sure."
-    elif all_uppercase:
+    elif phrase.isupper():
         # All uppercase
         return "Whoa, chill out!"
+    elif phrase.endswith('?'):
+        # '?' at end
+        return "Sure."
     else:
         return "Whatever."
