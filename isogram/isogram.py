55c88fb5ef54544ba286515ed2c2b8f0639c1e3c
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-04-10 10:15:22 +0100

    isogram: call lower() on the whole string

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,6 +1,6 @@
 def is_isogram(string):
     # Extract only alphabetic chars, as lower case.
-    all_chars = [ch.lower() for ch in string if ch.isalpha()]
+    all_chars = [ch for ch in string.lower() if ch.isalpha()]
 
     # No duplicates in a set, so test for equal lengths.
     return len(all_chars) == len(set(all_chars))
