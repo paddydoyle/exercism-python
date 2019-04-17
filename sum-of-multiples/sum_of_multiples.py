0c183edecaccce76ba8ecbf169e1ca0bf751d1ad
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-04-17 19:38:14 +0100

    sum_of_multiples: solution

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,16 @@
-def sum_of_multiples(limit, multiples):
-    pass
+def sum_of_multiples(limit, factors):
+    multiples = set()
+
+    for factor in factors:
+        # Special case: if factor is 0
+        if not factor:
+            continue
+
+        # All multiples of factor
+        i = factor
+        while i < limit:
+            # But only keep unique multiples, over all values in factor list.
+            multiples.add(i)
+            i += factor
+
+    return sum(multiples)
