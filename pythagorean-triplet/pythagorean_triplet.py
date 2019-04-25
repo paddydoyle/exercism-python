e7939386a8045cd0d4222fa7a62da6f90cd9194b
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-04-25 23:18:17 +0100

    pythagorean-triplet: initial solution; works but horribly slow, 95 seconds

--- pythagorean-triplet/pythagorean_triplet.py
+++ pythagorean-triplet/pythagorean_triplet.py
@@ -1,5 +1,21 @@
 def triplets_with_sum(sum_of_triplet):
-    pass
+    # set([(3, 4, 5)])
+
+    triplets = set()
+
+    for a in range(1, sum_of_triplet):
+        for b in range(a, (sum_of_triplet) // 2):
+            c = sum_of_triplet - a - b
+
+            triplet = (a, b, c)
+
+            # print("a = {}; b = {}; c = {}; sum = {}; is_triplet? {}"
+            # .format(a, b, c, (a + b + c), is_triplet(triplet)))
+
+            if is_triplet(triplet):
+                triplets.add(triplet)
+
+    return triplets
 
 
 def triplets_in_range(range_start, range_end):
@@ -7,4 +23,7 @@ def triplets_in_range(range_start, range_end):
 
 
 def is_triplet(triplet):
-    pass
+    # Assumes 'triplet' is a tuple
+    (a, b, c) = triplet
+
+    return a**2 + b**2 == c**2
