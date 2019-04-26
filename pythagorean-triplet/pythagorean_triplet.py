41ad0f55d9e965ac256736e5c2558cd56c7fb4aa
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-04-26 19:54:57 +0100

    pythagorean_triplet: improved range for b; down to 62 seconds
    Better formulation knowing that a < b < c
    Still far too long.

--- pythagorean-triplet/pythagorean_triplet.py
+++ pythagorean-triplet/pythagorean_triplet.py
@@ -1,19 +1,30 @@
+# https://en.wikipedia.org/wiki/Pythagorean_triple#Enumeration_of_primitive_Pythagorean_triples
 def triplets_with_sum(sum_of_triplet):
     # set([(3, 4, 5)])
 
     triplets = set()
+    # all_triplets = set()
 
     for a in range(1, sum_of_triplet):
-        for b in range(a, (sum_of_triplet) // 2):
+        # Fix a, and range b up to half of the sum_of_triplet
+        # to reduce duplicates.
+        for b in range(a + 1, (sum_of_triplet - a + 1) // 2):
             c = sum_of_triplet - a - b
 
             triplet = (a, b, c)
 
+            # if triplet in all_triplets:
+            #     print("FOUND duplicate {}".format(triplet))
+
+            # all_triplets.add(triplet)
+
             # print("a = {}; b = {}; c = {}; sum = {}; is_triplet? {}"
-            # .format(a, b, c, (a + b + c), is_triplet(triplet)))
+            #         .format(a, b, c, (a + b + c), is_triplet(triplet)))
 
             if is_triplet(triplet):
                 triplets.add(triplet)
+
+    # print("len(ALL) = {}".format(len(all_triplets)))
 
     return triplets
 
