a7a768cd2b4e2419427f11c664c22745121f0b4a
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-04-28 15:08:07 +0100

    saddle_points: small tidy up and better variable names

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -4,7 +4,7 @@ def saddle_points(matrix):
         return set()
 
     # Validate the matrix dimensions (unequal number of columns)
-    if not validate(matrix):
+    if len(set([len(row) for row in matrix])) != 1:
         raise ValueError('Not a regular matrix')
 
     # Find the max values in each row.
@@ -19,15 +19,8 @@ def saddle_points(matrix):
 
     # Loop over the maxes and mins, and when they are equal we
     # add the indexes (plus 1, because it starts counting at 1, not 0)
-    # to the set.
+    # as a tuple to the set.
     return set((i+1, j+1)
-               for i, r in enumerate(row_max_vals)
-               for j, c in enumerate(col_min_vals)
-               if r == c)
-
-
-def validate(matrix):
-    # Check the lengths of each row (i.e. the number of columns).
-    # Add them to a set and if the matrix is irregular, there
-    # will be more than one value in the set.
-    return len(set([len(row) for row in matrix])) == 1
+               for i, row_max in enumerate(row_max_vals)
+               for j, col_min in enumerate(col_min_vals)
+               if row_max == col_min)
