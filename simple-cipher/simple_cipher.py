0546a8ef5d22cfec88d9812ec118f49e6b2e3683
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-05-08 22:15:37 +0100

    simple-cipher: comments; rename vars

--- simple-cipher/simple_cipher.py
+++ simple-cipher/simple_cipher.py
@@ -13,7 +13,7 @@ class Cipher(object):
 
             self.key = ''.join(random.choices(string.ascii_lowercase, k=100))
 
-        # Instead of just storing the key, more efficient
+        # Instead of just storing the key, more time efficient
         # to convert that directly into a list of offsets and
         # store those as well.
         self.offsets = [ord(k) - ord('a') for k in self.key]
@@ -21,26 +21,31 @@ class Cipher(object):
     def encode(self, plaintext):
         cipher = []
 
-        for i, p in enumerate(plaintext):
+        for i, ch in enumerate(plaintext):
             # If the text is longer than the key
             key_index = i % len(self.key)
 
             # Positive offset
-            cipher.append(self._shift_lower_ascii(p, self.offsets[key_index]))
+            cipher.append(self._shift_lower_ascii(ch, self.offsets[key_index]))
 
         return ''.join(cipher)
 
     def decode(self, ciphertext):
         plain = []
 
-        for i, p in enumerate(ciphertext):
+        for i, ch in enumerate(ciphertext):
             # If the text is longer than the key
             key_index = i % len(self.key)
 
             # Negative offset
-            plain.append(self._shift_lower_ascii(p, -self.offsets[key_index]))
+            plain.append(self._shift_lower_ascii(ch, -self.offsets[key_index]))
 
         return ''.join(plain)
 
     def _shift_lower_ascii(self, char, shift):
+        # Get ord of the character.
+        # Add the shift, then subtract ord('a') to get a value starting from 0.
+        # Reduce that mod 26 for the 26 chars in the latin alphabet.
+        # Then add back ord('a') to bring to the range ord(a) - ord(z)
+        # And finally turn that back into the char.
         return chr(ord('a') + ((ord(char) + shift) - ord('a')) % 26)
