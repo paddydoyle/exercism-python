a40344a3ea4feded2668af287a37dd43c2c8a31a
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-05-08 22:03:02 +0100

    simple-cipher: working; needs tidying

--- simple-cipher/simple_cipher.py
+++ simple-cipher/simple_cipher.py
@@ -1,9 +1,53 @@
+import random
+import string
+
+
 class Cipher(object):
     def __init__(self, key=None):
-        pass
+        self.key = key
 
-    def encode(self, text):
-        pass
+        # If no key is supplied, generate a random string of 100 lowercase
+        if not self.key:
+            # Reset the RNG, to ensure a unique name even if an external
+            random.seed()
 
-    def decode(self, text):
-        pass
+            self.key = ''.join(random.choices(string.ascii_lowercase, k=100))
+
+        # Instead of just storing the key, more efficient
+        # to convert that directly into a list of offsets and
+        # store those.
+
+        self.offsets = [ord(k) - ord('a') for k in self.key]
+
+    def encode(self, plaintext):
+        # Trivial case
+        if not self.key:
+            return plaintext
+
+        cipher = []
+        for i, p in enumerate(plaintext):
+            # If the text is longer than the key
+            key_index = i % len(self.key)
+
+            # Positive offset
+            cipher.append(self._shift_lower_ascii(p, self.offsets[key_index]))
+
+        return ''.join(cipher)
+
+    def decode(self, ciphertext):
+        # Trivial case
+        if not self.key:
+            return ciphertext
+
+        plain = []
+        for i, p in enumerate(ciphertext):
+            # If the text is longer than the key
+            key_index = i % len(self.key)
+
+            # Negative offset
+            plain.append(self._shift_lower_ascii(p, -self.offsets[key_index]))
+
+        return ''.join(plain)
+
+    def _shift_lower_ascii(self, char, shift):
+        return chr(ord('a') + ((ord(char) + shift) - ord('a')) % 26)
