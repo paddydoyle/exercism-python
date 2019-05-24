efd5f68c9da10fb60510b0ce1a235cc6ed708228
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-05-24 23:31:55 +0100

    triangle: working

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,10 +1,47 @@
 def equilateral(sides):
-    pass
+    if not _verify_triangle(sides):
+        return False
+
+    # For convenience
+    (a, b, c) = sides
+
+    return a == b == c
 
 
 def isosceles(sides):
-    pass
+    if not _verify_triangle(sides):
+        return False
+
+    # For convenience
+    (a, b, c) = sorted(sides)
+
+    return a == b or b == c
 
 
 def scalene(sides):
-    pass
+    if not _verify_triangle(sides):
+        return False
+
+    # For convenience
+    (a, b, c) = sorted(sides)
+
+    return a != b and b != c
+
+
+def _verify_triangle(sides):
+    """
+    Common triangle verification checks.
+    """
+
+    # All zero sides
+    if not any(sides):
+        return False
+
+    # For convenience
+    (a, b, c) = sorted(sides)
+
+    # Check triangle inequality
+    if a + b < c:
+        return False
+
+    return True
