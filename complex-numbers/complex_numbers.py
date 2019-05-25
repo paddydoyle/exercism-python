d64841577eb0a0838b4ecf0332b5aeb423aa537f
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-05-25 21:55:51 +0100

    complex-numbers: working

--- complex-numbers/complex_numbers.py
+++ complex-numbers/complex_numbers.py
@@ -1,24 +1,53 @@
+import math
+
+
 class ComplexNumber(object):
     def __init__(self, real, imaginary):
-        pass
+        self.real = real
+        self.imaginary = imaginary
 
     def __add__(self, other):
-        pass
+        # (a + c) + (b + d) * i
+        real = self.real + other.real
+        imaginary = self.imaginary + other.imaginary
+        return ComplexNumber(real, imaginary)
 
     def __mul__(self, other):
-        pass
+        # (a * c - b * d) + (b * c + a * d) * i
+        real = (self.real * other.real - self.imaginary * other.imaginary)
+        imaginary = (self.imaginary * other.real + self.real * other.imaginary)
+        return ComplexNumber(real, imaginary)
 
     def __sub__(self, other):
-        pass
+        # (a - c) + (b - d) * i
+        real = self.real - other.real
+        imaginary = self.imaginary - other.imaginary
+        return ComplexNumber(real, imaginary)
 
     def __truediv__(self, other):
-        pass
+        # (a * c + b * d)/(c^2 + d^2) + (b * c - a * d)/(c^2 + d^2) * i
+        divisor = (other.real**2 + other.imaginary**2)
+        real = ((self.real * other.real + self.imaginary * other.imaginary) /
+                divisor)
+        imaginary = ((self.imaginary * other.real -
+                      self.real * other.imaginary) /
+                     divisor)
+        return ComplexNumber(real, imaginary)
 
     def __abs__(self):
-        pass
+        # sqrt(a^2 + b^2)
+        return math.sqrt(self.imaginary**2 + self.real**2)
 
     def conjugate(self):
-        pass
+        # a - b * i
+        return ComplexNumber(self.real, -self.imaginary)
 
     def exp(self):
-        pass
+        # exp(a) * exp(i * b), with
+        # exp(i * b) = cos(b) + i * sin(b)
+        real = math.exp(self.real) * math.cos(self.imaginary)
+        imaginary = math.exp(self.real) * math.sin(self.imaginary)
+        return ComplexNumber(real, imaginary)
+
+    def __eq__(self, other):
+        return self.real == other.real and self.imaginary == other.imaginary
