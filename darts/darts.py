9b603abd1ed4f6ad2c64ce2dc8d97f76e319599e
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-05-31 22:26:29 +0100

    darts: initial solution

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,25 @@
+import math
+
+
 def score(x, y):
-    pass
+    # List of tuples:
+    # - the first element is the minimum radius
+    # - the second element is the corresponding score
+    # Order them in decreasing radius.
+    scores = [
+            (10, 0),
+            (5, 1),
+            (1, 5),
+            (0, 10)
+            ]
+
+    # Euclidean distance from origin.
+    dist = math.sqrt(x**2 + y**2)
+
+    # Search from out to in, returning first score where
+    # the point distance is beyond that radius.
+    for radius, score in scores:
+        if dist > radius:
+            return score
+
+    return 0
