8921cbe51f58d03bd6c2e5237a39d71ffea526c7
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-05-31 22:02:11 +0100

    collatz_conjecture: solution

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,19 @@
 def steps(number):
-    pass
+    if number < 1:
+        raise ValueError("Stictly positive inputs only")
+
+    count = 0
+
+    # Assuming the conjecture is true, so no additional
+    # guard condition!
+    while number != 1:
+        if number % 2 == 0:
+            # Even, return n/2
+            number = number / 2
+        else:
+            # Odd, return 3n+1
+            number = 3 * number + 1
+
+        count += 1
+
+    return count
