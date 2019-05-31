4112a30f481651f1c46658596846e76986b66379
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-05-31 22:06:53 +0100

    collatz-conjecture: add a max_iter

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,19 +1,21 @@
 def steps(number):
+    MAX_ITER = 2**20
+
     if number < 1:
         raise ValueError("Stictly positive inputs only")
 
-    count = 0
-
-    # Assuming the conjecture is true, so no additional
-    # guard condition!
-    while number != 1:
-        if number % 2 == 0:
+    # Probably overkill to include a max iteration.
+    for i in range(MAX_ITER):
+        if number == 1:
+            break
+        elif number % 2 == 0:
             # Even, return n/2
             number = number / 2
         else:
             # Odd, return 3n+1
             number = 3 * number + 1
 
-        count += 1
+    if i == MAX_ITER:
+        raise ValueError("Hit MAX_ITER. Bailing out!")
 
-    return count
+    return i
