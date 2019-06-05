156ca7456e8f1b0e6fd220a945b5fb9d93151148
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-06-05 21:00:36 +0100

    resistor-color: move the list into a function

--- resistor-color/resistor_color.py
+++ resistor-color/resistor_color.py
@@ -1,22 +1,19 @@
-# List of colours.
-# Could use list of (colour, number) tuples instead.
-color_list = [
-    'black',
-    'brown',
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'violet',
-    'grey',
-    'white'
-]
 
 
 def color_code(color):
-    return color_list.index(color)
+    return colors().index(color)
 
 
 def colors():
-    return color_list
+    return [
+        'black',
+        'brown',
+        'red',
+        'orange',
+        'yellow',
+        'green',
+        'blue',
+        'violet',
+        'grey',
+        'white'
+    ]
