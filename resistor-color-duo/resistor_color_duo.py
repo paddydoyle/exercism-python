8a9961fa3170d15b8cb76c318fbc75b862be9fdc
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-06-05 21:06:10 +0100

    resistor-color-duo: solution

--- resistor-color-duo/resistor_color_duo.py
+++ resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,23 @@
 def value(colors):
-    pass
+    return int(''.join(str(_color_code(color)) for color in colors))
+
+
+# Re-use code from previous exercise
+def _color_code(color):
+    return _colors().index(color)
+
+
+# Re-use code from previous exercise
+def _colors():
+    return [
+        'black',
+        'brown',
+        'red',
+        'orange',
+        'yellow',
+        'green',
+        'blue',
+        'violet',
+        'grey',
+        'white'
+    ]
