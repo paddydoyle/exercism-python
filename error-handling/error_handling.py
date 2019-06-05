ab9db3e6fd613d014ce9f7237c3f9642f67a41f1
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-06-05 20:30:26 +0100

    error-handling: solution

--- error-handling/error_handling.py
+++ error-handling/error_handling.py
@@ -1,14 +1,28 @@
 def handle_error_by_throwing_exception():
-    pass
+    error = True
+    if error:
+        raise Exception("Raising an Exception")
 
 
 def handle_error_by_returning_none(input_data):
-    pass
+    # Really not sure if this what is wanted.
+    if input_data == '1':
+        return 1
+    else:
+        return None
 
 
 def handle_error_by_returning_tuple(input_data):
-    pass
+    # Really not sure if this what is wanted.
+    if input_data == '1':
+        return (True, 1)
+    else:
+        return (False, None)
 
 
 def filelike_objects_are_closed_on_exception(filelike_object):
-    pass
+    # Took me a very long time to figure this out from the test cases.
+    with filelike_object:
+        filelike_object.open()
+        filelike_object.do_something()
+        filelike_object.close()
