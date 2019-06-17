d161f157f67d749c95e28958b2996f55c010dafa
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-06-17 22:34:21 +0100

    twleve_days: mentor feedback
    - use CONSTANTS outside of the function
    - remove slight repeition in the verses array
    - use a token in the last line; not sure about this one, if that's what
      they are hinting at or not.

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -1,59 +1,63 @@
+# Ordinal days. The package num2words could be used here.
+ORDINAL_DAYS = [None,
+                "first",
+                "second",
+                "third",
+                "fourth",
+                "fifth",
+                "sixth",
+                "seventh",
+                "eighth",
+                "ninth",
+                "tenth",
+                "eleventh",
+                "twelfth"]
+
+# Reverse order, to make the array slices easier. Also makes it
+# easier to add 'thirteenth' etc, and the array slice below doesn't
+# have to change.
+# Include a token for adding 'and ' if required for verse 1.
+VERSES = ["twelve Drummers Drumming",
+          "eleven Pipers Piping",
+          "ten Lords-a-Leaping",
+          "nine Ladies Dancing",
+          "eight Maids-a-Milking",
+          "seven Swans-a-Swimming",
+          "six Geese-a-Laying",
+          "five Gold Rings",
+          "four Calling Birds",
+          "three French Hens",
+          "two Turtle Doves",
+          "{}a Partridge in a Pear Tree."]
+
+# Split off first line; we always use that. Include a token
+# for the ordinal day as a word.
+# Template for the verse, including a token for the ordinal day
+# and the gift string.
+VERSE_TEMPLATE = "On the {} day of Christmas my true love gave to me: {}"
+
+
 def recite(start_verse, end_verse):
-    # Ordinal days. The package num2words could be used here.
-    days = ["zeroth",
-            "first",
-            "second",
-            "third",
-            "fourth",
-            "fifth",
-            "sixth",
-            "seventh",
-            "eighth",
-            "ninth",
-            "tenth",
-            "eleventh",
-            "twelfth"]
-
-    # Split off first line; we always use that. Include a token
-    # for the ordinal day as a word.
-    first_line = "On the {} day of Christmas my true love gave to me: "
-
-    # Split off last line for verse one, which does not include the
-    # word 'and'. This is duplicated in the array, but simplifies
-    # the array slice.
-    last_line_verse_one = "a Partridge in a Pear Tree."
-
-    # Reverse order, to make the array slices easier. Also makes it
-    # easier to add 'thirteenth' etc, and the array slice below doesn't
-    # have to change.
-    verses = ["twelve Drummers Drumming, ",
-              "eleven Pipers Piping, ",
-              "ten Lords-a-Leaping, ",
-              "nine Ladies Dancing, ",
-              "eight Maids-a-Milking, ",
-              "seven Swans-a-Swimming, ",
-              "six Geese-a-Laying, ",
-              "five Gold Rings, ",
-              "four Calling Birds, ",
-              "three French Hens, ",
-              "two Turtle Doves, ",
-              "and " + last_line_verse_one]
-
     # Calculate once, for convenience.
-    n_verses = len(verses)
+    n_verses = len(VERSES)
 
     lyrics = []
 
     # Add 1 because the inputs start to count at 1, not 0
-    for verse in range(start_verse, end_verse + 1):
+    for verse_num in range(start_verse, end_verse + 1):
 
-        day = days[verse]
+        ordinal_day = ORDINAL_DAYS[verse_num]
 
-        if verse == 1:
-            lyrics.append(first_line.format(day) + last_line_verse_one)
+        if verse_num == 1:
+            bridge = ""
         else:
-            # Slice the last 'verse' verses.
-            lyrics.append(first_line.format(day) +
-                          ''.join(verses[n_verses - verse:]))
+            bridge = "and "
+
+        # Slice the last 'verse_num' lines of verses.
+        # Add the bridge word if required, using the token in last
+        # entry of VERSES.
+        gift_string = ", ".join(VERSES[n_verses - verse_num:]).format(bridge)
+
+        lyrics.append(VERSE_TEMPLATE.format(ordinal_day, gift_string))
 
     return lyrics
