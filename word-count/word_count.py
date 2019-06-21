96b48cd06b91798f2b5b16d31d68e9d782f63e79
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-06-21 21:06:01 +0100

    word_count: back to basic idiom

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -3,6 +3,9 @@ from collections import Counter
 
 
 def count_words(sentence):
+    # Note: Cound use collections.Counter, but it's slower because of
+    # the need to convert it back to a dict() to match the test results.
+
     count = {}
 
     # Allow single quote inside the word only.
@@ -10,4 +13,10 @@ def count_words(sentence):
 
     words = pattern.findall(sentence.lower())
 
-    return dict(Counter(words).most_common())
+    for word in words:
+        if word not in count:
+            count[word] = 1
+        else:
+            count[word] += 1
+
+    return count
