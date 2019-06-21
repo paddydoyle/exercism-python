991133a6464c698e7e0055e3ccd267020332df62
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-06-21 20:47:01 +0100

    word_count: works

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,19 @@
+import re
+
+
 def count_words(sentence):
-    pass
+    count = {}
+
+    # Allow single quote inside the word only.
+    pattern = re.compile(r"([a-z\d]+(?:'[a-z\d]+)?)")
+
+    words = pattern.findall(sentence.lower())
+
+    for word in words:
+        # Is there a better idiom here?
+        if word not in count:
+            count[word] = 1
+        else:
+            count[word] += 1
+
+    return count
