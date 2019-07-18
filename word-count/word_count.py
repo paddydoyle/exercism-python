88beb190cd2ea2deffff5f750cefa418b89cb2db
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-07-18 16:28:04 +0100

    word-count: helper const for the regex

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -3,11 +3,10 @@ from collections import Counter
 
 
 def count_words(sentence):
-    # Note: Cound use collections.Counter, but it's slower because of
-    # the need to convert it back to a dict() to match the test results.
+    RE_WORD_INCL_SINGLE_QUOTE = r"([a-z\d]+(?:'[a-z\d]+)?)"
 
     # Allow single quote inside the word only.
-    pattern = re.compile(r"([a-z\d]+(?:'[a-z\d]+)?)")
+    pattern = re.compile(RE_WORD_INCL_SINGLE_QUOTE)
 
     words = pattern.findall(sentence.lower())
 
