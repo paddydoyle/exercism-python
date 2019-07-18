dc88d43ba644500b83dddc4c87cc78d884ab9c05
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-07-18 16:27:25 +0100

    word-count: aah, can just use Counter() (subclass of dict) and don't need most_common()

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -6,14 +6,9 @@ def count_words(sentence):
     # Note: Cound use collections.Counter, but it's slower because of
     # the need to convert it back to a dict() to match the test results.
 
-    count = {}
-
     # Allow single quote inside the word only.
     pattern = re.compile(r"([a-z\d]+(?:'[a-z\d]+)?)")
 
     words = pattern.findall(sentence.lower())
 
-    for word in words:
-        count[word] = count.get(word, 0) + 1
-
-    return count
+    return Counter(words)
