2a895dc1431770679b60846f7c38721dfbe7d4d1
Paddy Doyle <paddy.doyle@gmail.com>
2020-06-23 22:22:08 +0100

    scrabble_score: use list of tuples to avoid overhead of dict

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,16 +1,16 @@
 # Group the char together by score, minimising repetition.
-SCORE_TO_CHAR = {
-        1: 'AEIOULNRST',
-        2: 'DG',
-        3: 'BCMP',
-        4: 'FHVWY',
-        5: 'K',
-        8: 'JX',
-        10: 'QZ',
-        }
+SCORE_TO_CHAR = [
+        (1, 'AEIOULNRST'),
+        (2, 'DG'),
+        (3, 'BCMP'),
+        (4, 'FHVWY'),
+        (5, 'K'),
+        (8, 'JX'),
+        (10, 'QZ'),
+        ]
 
 # Invert the above, exploding the strings as we go.
-CHAR_TO_SCORE = {char: score for score, chars in SCORE_TO_CHAR.items()
+CHAR_TO_SCORE = {char: score for score, chars in SCORE_TO_CHAR
                  for char in chars}
 
 
