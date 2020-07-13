468674c75b1bfc38d26095378a85af9db5c71478
Paddy Doyle <paddy.doyle@gmail.com>
2020-07-13 21:50:34 +0100

    kindergarten-garden: pep8 fixes

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -24,11 +24,15 @@ STUDENTS = [
 
 
 class Garden:
-    def __init__(self, diagram, students = STUDENTS):
-        # The student list may not be sorted initially
-        self.students = sorted(students)
+    def __init__(self, diagram, students=None):
+        # Deal with dangerous-default-value issue
+        if not students:
+            self.students = STUDENTS
+        else:
+            # The student list may not be sorted initially
+            self.students = sorted(students)
 
-        # Parse the diagram once initially
+        # Parse the diagram once. Allow for different numbers of rows
         rows = diagram.split()
 
         # Verify that the rows are equal sizes
@@ -39,9 +43,11 @@ class Garden:
         self.rows = rows
 
     def plants(self, student):
-        if not student in self.students:
-            raise Exception("Requested student {} not in the list: {}".format(student, self.students))
+        if student not in self.students:
+            raise Exception("Requested student {} not in the list: {}"
+                            "".format(student, self.students))
 
         index = self.students.index(student) * PLANTS_PER_ROW
 
-        return [PLANTS[ch] for row in self.rows for ch in row[index:index+PLANTS_PER_ROW]]
+        return [PLANTS[ch] for row in self.rows
+                for ch in row[index:index+PLANTS_PER_ROW]]
