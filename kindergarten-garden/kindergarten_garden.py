7c859e1b54caeb30fc0f74a8d9e24ebf15b749b0
Paddy Doyle <paddy.doyle@gmail.com>
2020-07-20 21:43:09 +0100

    kindergarten-garden: no longer need both student list and student map

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -26,9 +26,9 @@ STUDENTS = [
 class Garden:
     def __init__(self, diagram, students=None):
         if not students:
-            self.students = STUDENTS
+            students = STUDENTS
         else:
-            self.students = sorted(students)
+            students = sorted(students)
 
         rows = diagram.split()
 
@@ -36,15 +36,15 @@ class Garden:
 
         self.student_map = {student: self._gen_plants(student,
                                                       index * PLANTS_PER_ROW)
-                            for (index, student) in enumerate(self.students)}
+                            for (index, student) in enumerate(students)}
 
     def _gen_plants(self, student, index):
         return [PLANTS[ch] for row in self.rows
                 for ch in row[index:index+PLANTS_PER_ROW]]
 
     def plants(self, student):
-        if student not in self.students:
+        if student not in self.student_map:
             raise Exception("Requested student {} not in the list: {}"
-                            "".format(student, self.students))
+                            "".format(student, keys(self.student_map)))
 
         return self.student_map[student]
